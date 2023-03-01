12290d56797e780fe1b8cd5c03a7b7aef8ed89c2
SpriteCT <124687778+SpriteCT@users.noreply.github.com>
2023-03-02 00:14:41 +0300

    main

---
+++ ProgrammingTechnology/ProgrammingTechnology/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProgrammingTechnology
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Vasya = new Student("Vasya", "Pupkin", 300);
+            Vasya.GetInfo();
+            Student Dasha = new Student("Dasha", "Anonimka", 270);
+            Dasha.GetInfo();
+            Student Grisha = new Student("Grisha", 130);
+            Grisha.GetInfo();
+            Console.WriteLine($"У Даши шансов поступить больше, чем у Васи >> {Dasha.IsChanceBigger(Vasya)}");
+            Console.WriteLine($"У Васи шансов поступить больше, чем у Гриши >> {Vasya.IsChanceBigger(Grisha)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProgrammingTechnology/ProgrammingTechnology/Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ProgrammingTechnology
+{
+    public class Student
+    {
+        private string name;
+        private string surName;
+        private int score;
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    score = value;
+                }
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string SurName
+        {
+            get
+            {
+                return surName;
+            }
+            set
+            {
+                surName = value;
+            }
+        }
+
+        public Student(string Name, string SurName, int Score)
+        {
+            this.SurName = SurName;
+            this.Name = Name;
+            this.Score = Score;
+        }
+        public Student(string Name, string SurName) : this(Name, SurName, 0)
+        {
+        }
+        public Student(string Name) : this(Name, "")
+        {
+        }
+        public Student(string Name, int Score) : this(Name, "", Score)
+        {
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя >> {name}");
+            Console.WriteLine($"Фамилия >> {surName}");
+            Console.WriteLine($"Кол-во баллов >> {score}\n");
+        }
+        public bool IsChanceBigger(Student student)
+        {
+            return this.score > student.Score;
+        }
+
+    }
+}
